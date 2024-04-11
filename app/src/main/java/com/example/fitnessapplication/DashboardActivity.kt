1e5cbaf7750e6794a7eaacea748242ff1251b9df
FedericoM120 <monteverdi.102@gmail.com>
2024-04-11 16:18:01 -0400

    commit

---
+++ app/src/main/java/com/example/fitnessapplication/DashboardActivity.kt
@@ -0,0 +1,18 @@
+package com.example.fitnessapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fitnessapplication.databinding.ActivityDashboardBinding
+
+class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize your buttons and other UI elements here
+        //comments
+    }
+}
--- app/src/main/java/com/example/fitnessapplication/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapplication/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.fitnessapplication
 
-import android.util.Log
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fitnessapplication.databinding.ActivityMainBinding
@@ -16,14 +16,10 @@ class MainActivity : AppCompatActivity() {
         binding.loginButton.setOnClickListener {
             val username = binding.username.text.toString()
             val password = binding.password.text.toString()
-            // You can now use username and password to authenticate the user
-            // For now, we'll just print them to log
+
             if (username == "admin" && password == "admin") { // Example validation
-                // Proceed to next screen or show success
-                Log.d("LoginActivity", "Login successful")
-            } else {
-                // Show error or retry
-                Log.d("LoginActivity", "Login failed")
+                val intent = Intent(this, DashboardActivity::class.java)
+                startActivity(intent)
             }
         }
     }
